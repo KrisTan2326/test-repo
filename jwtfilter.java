2da0367fdab69d5bf39878a67db7c90477d85a7d
KrisTan2326 <128054270+KrisTan2326@users.noreply.github.com>
2025-09-27 03:17:54 +0530

    Update jwtfilter.java

--- jwtfilter.java
+++ jwtfilter.java
@@ -4,8 +4,6 @@ import com.hashedin.huspark.service.MyUserDetailsService;
 import jakarta.servlet.FilterChain;
 
 
-
-
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
