4ca5398c7038cc24650225cd7f80d32e3e1022fe
KrisTan2326 <128054270+KrisTan2326@users.noreply.github.com>
2025-09-28 19:56:03 +0530

    Update CourseController.java

--- CourseController.java
+++ CourseController.java
@@ -1,5 +1,7 @@
 package com.hashedin.huspark.controller;
 
+
+
 import com.hashedin.huspark.model.Course;
 import com.hashedin.huspark.model.Users;
 import com.hashedin.huspark.repository.CourseRepository;
